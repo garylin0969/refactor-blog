d82789fb7717dcb465a4c159ff6c75008257a710
Gary <garylin0969@gmail.com>
2025-07-17 19:38:16 +0800

    feat: add velite configuration file to define project structure and collections for content management

---
+++ velite.config.ts
@@ -0,0 +1,30 @@
+import { defineConfig, s } from 'velite';
+
+export default defineConfig({
+    // 設定根目錄
+    root: './content',
+
+    // 設定輸出目錄
+    output: {
+        data: '.velite',
+        assets: 'public/static',
+        base: '/static/',
+        name: '[name]-[hash:6].[ext]',
+        clean: true,
+    },
+
+    // 定義collections
+    collections: {
+        posts: {
+            name: 'Post',
+            pattern: 'posts/**/*.{md,mdx}',
+            schema: s.object({
+                title: s.string(),
+                date: s.isodate(),
+                description: s.string().optional(),
+                slug: s.slug('title'),
+                code: s.mdx(),
+            }),
+        },
+    },
+});
