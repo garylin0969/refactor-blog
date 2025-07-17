35ce02695275c53bd8f15eb2e267e8f96ba35a3b
Gary Lin <garylin0969@gmail.com>
2025-07-17 10:48:36 +0800

    refactor: replace AboutPage content with AuthorCard component for improved modularity and code organization

---
+++ src/components/molecules/author-card/author-card.tsx
@@ -0,0 +1,42 @@
+import { MapPinIcon, MailIcon } from 'lucide-react';
+import BaseAvatar from '@/components/atoms/base-avatar';
+import SocialLinks from '@/components/molecules/social-links';
+import { Card, CardContent } from '@/components/ui/card';
+import { cn } from '@/utils/shadcn';
+
+const AUTHOR_INFO = {
+    name: 'Gary Lin',
+    avatar: 'https://github.com/shadcn.png',
+    location: 'Taipei, Taiwan',
+    email: 'garylin0969@gmail.com',
+} as const;
+
+interface AuthorCardProps {
+    className?: string;
+}
+
+const AuthorCard = ({ className }: AuthorCardProps) => {
+    return (
+        <Card className={cn('w-74', className)}>
+            <CardContent className="space-y-3">
+                <BaseAvatar className="mx-auto size-33" src={AUTHOR_INFO.avatar} fallback="author" />
+                <div className="text-center text-2xl font-bold">{AUTHOR_INFO.name}</div>
+                <ul className="flex flex-col items-center gap-y-2">
+                    <li className="flex items-center gap-x-2">
+                        <MapPinIcon className="size-4" />
+                        <span>{AUTHOR_INFO.location}</span>
+                    </li>
+                    <li className="flex items-center gap-x-2">
+                        <MailIcon className="size-4" />
+                        <a href={`mailto:${AUTHOR_INFO.email}`} target="_blank" rel="noreferrer noopener">
+                            <span>{AUTHOR_INFO.email}</span>
+                        </a>
+                    </li>
+                </ul>
+                <SocialLinks className="flex items-center justify-center gap-x-2" iconClassName="size-6" />
+            </CardContent>
+        </Card>
+    );
+};
+
+export default AuthorCard;
