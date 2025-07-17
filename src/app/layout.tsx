6c936700a3cb6d8de59ed753a230f4b4793a2d81
Gary Lin <garylin0969@gmail.com>
2025-07-17 08:12:12 +0800

    style: adjust main layout padding for improved content presentation

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -32,7 +32,7 @@ export default function RootLayout({
                 <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
                     <div className="flex min-h-screen flex-col">
                         <Header />
-                        <main className="flex-1">{children}</main>
+                        <main className="container mx-auto flex-1 px-4">{children}</main>
                     </div>
                 </ThemeProvider>
             </body>
