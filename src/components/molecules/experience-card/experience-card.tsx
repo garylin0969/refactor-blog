69bfc88c5f117d9561d8fa1120ea2ca4d86771ad
Gary Lin <garylin0969@gmail.com>
2025-07-17 15:19:08 +0800

    feat: create ExperienceCard component for displaying work experience details with Accordion functionality

---
+++ src/components/molecules/experience-card/experience-card.tsx
@@ -0,0 +1,42 @@
+import NextImage from '@/components/atoms/next-image';
+import { Accordion, AccordionContent, AccordionItem, AccordionTrigger } from '@/components/ui/accordion';
+import { Button } from '@/components/ui/button';
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card';
+import type { ExperienceItem } from '@/constants/experience';
+
+interface ExperienceCardProps {
+    experience: ExperienceItem;
+}
+
+const ExperienceCard = ({ experience }: ExperienceCardProps) => (
+    <Card className="gap-y-2 p-2.5">
+        <CardHeader className="flex items-center gap-x-2 p-0">
+            <NextImage className="rounded-md" src={experience.image} alt={experience.company} width={64} height={64} />
+            <div className="space-y-1">
+                <CardTitle>{experience.title}</CardTitle>
+                <CardDescription>{experience.company}</CardDescription>
+                <CardDescription>
+                    {experience.startDate} - {experience.endDate}
+                </CardDescription>
+            </div>
+        </CardHeader>
+        <CardContent className="p-0">
+            <Accordion type="single" collapsible>
+                <AccordionItem value={experience.id}>
+                    <AccordionTrigger asChild>
+                        <Button className="w-full">View Details</Button>
+                    </AccordionTrigger>
+                    <AccordionContent className="p-0">
+                        <ul className="list-inside list-disc space-y-2 p-3 text-base">
+                            {experience.details.map((detail, detailIndex) => (
+                                <li key={detailIndex}>{detail}</li>
+                            ))}
+                        </ul>
+                    </AccordionContent>
+                </AccordionItem>
+            </Accordion>
+        </CardContent>
+    </Card>
+);
+
+export default ExperienceCard;
