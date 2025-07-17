57cb4e5bcc739d89e2b2cfda0a00b820a84a72b3
Gary Lin <garylin0969@gmail.com>
2025-07-17 11:43:49 +0800

    feat: add detailed experience section to AboutPage with Card component for enhanced user presentation

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,6 +1,9 @@
+import NextImage from 'next/image';
 import SectionTitle from '@/components/atoms/section-title';
 import AuthorCard from '@/components/molecules/author-card';
 import IntroCard from '@/components/molecules/intro-card';
+import { Button } from '@/components/ui/button';
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card';
 
 const AboutPage = () => {
     return (
@@ -8,6 +11,19 @@ const AboutPage = () => {
             <AuthorCard className="mx-auto" />
             <IntroCard className="mx-auto max-w-3xl" />
             <SectionTitle className="text-center">Experience</SectionTitle>
+            <Card className="gap-2 p-2">
+                <CardHeader className="flex items-center gap-2 p-0">
+                    <NextImage src="/image-not-found.png" alt="Anue 鉅亨網" width={64} height={64} />
+                    <div>
+                        <CardTitle>Frontend Developer</CardTitle>
+                        <CardDescription>Anue 鉅亨網</CardDescription>
+                        <CardDescription>2024.03 - Now</CardDescription>
+                    </div>
+                </CardHeader>
+                <CardContent className="p-0">
+                    <Button className="w-full">View Details</Button>
+                </CardContent>
+            </Card>
         </div>
     );
 };
