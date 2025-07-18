1c767496e1b07c69fcda13ffbe389fa330ee6c8f
Gary Lin <garylin0969@gmail.com>
2025-07-18 11:09:37 +0800

    style: update PostPage to apply dark mode styling with the addition of dark:prose-invert class for improved readability in dark themes

--- src/app/blog/posts/[slug]/page.tsx
+++ src/app/blog/posts/[slug]/page.tsx
@@ -51,7 +51,7 @@ const PostPage = async ({ params }: PageProps) => {
                     <PostMeta date={post?.date} category={post?.category} />
                     <TagList tags={post?.tags ?? []} />
                 </header>
-                <div className="prose md:prose-lg">
+                <div className="prose dark:prose-invert md:prose-lg">
                     <MDXContent code={post?.code} />
                 </div>
             </article>
