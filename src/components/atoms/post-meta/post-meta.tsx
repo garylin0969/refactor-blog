d9dd898ac055c45fd00923b469981ce79c036828
Gary Lin <garylin0969@gmail.com>
2025-07-18 11:08:53 +0800

    refactor: enhance PostMeta component by adding className prop for customizable styling and improving layout with utility functions

--- src/components/atoms/post-meta/post-meta.tsx
+++ src/components/atoms/post-meta/post-meta.tsx
@@ -1,16 +1,18 @@
 import { Badge } from '@/components/ui/badge';
 import { formatDate } from '@/utils/date';
+import { cn } from '@/utils/shadcn';
 
 interface PostMetaProps {
+    className?: string;
     date: string;
     category?: string;
 }
 
-export const PostMeta = ({ date, category }: PostMetaProps) => {
+export const PostMeta = ({ className, date, category }: PostMetaProps) => {
     const formattedDate = formatDate(date, 'YYYY/MM/DD');
 
     return (
-        <div className="text-muted-foreground flex items-center gap-3 text-sm">
+        <div className={cn('text-muted-foreground flex items-center gap-x-3 text-sm', className)}>
             <span>{formattedDate}</span>
             {category && (
                 <>
