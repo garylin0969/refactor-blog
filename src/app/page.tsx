37d0cf0e58b91149ffa7fb9712bd43e44dcc329f
Gary <garylin0969@gmail.com>
2025-07-20 01:21:46 +0800

    refactor: update HomePage layout to use grid and flex for improved structure and alignment, while removing unused SectionTitle import

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,4 @@
 import Link from 'next/link';
-import SectionTitle from '@/components/atoms/section-title';
 import AuthorCard from '@/components/molecules/author-card';
 import BlogPostCard from '@/components/molecules/blog-post-card';
 import IntroCard from '@/components/molecules/intro-card';
@@ -14,36 +13,42 @@ const latestPosts = getLatestPosts(1);
 
 const HomePage = () => {
     return (
-        <div className="mx-auto max-w-6xl space-y-4 md:space-y-8">
-            <section className="space-y-4 md:space-y-8">
-                <div className="flex flex-wrap justify-center gap-4">
-                    <div className="flex justify-center">
-                        {/* 作者卡片 */}
-                        <AuthorCard />
+        <div className="grid flex-1 place-content-center">
+            <div className="mx-auto max-w-6xl space-y-4 md:space-y-8">
+                <section className="space-y-4 md:space-y-8">
+                    <div className="flex flex-wrap justify-center gap-4">
+                        <div className="flex justify-center">
+                            {/* 作者卡片 */}
+                            <AuthorCard />
+                        </div>
+                        <div className="hidden md:flex md:justify-center">
+                            <ProjectCard
+                                className="w-74"
+                                badge="New Project"
+                                maxVisible={4}
+                                project={PROJECT_LIST[0]}
+                            />
+                        </div>
                     </div>
-                    <div className="hidden md:flex md:justify-center">
-                        <ProjectCard className="w-74" badge="New Project" maxVisible={4} project={PROJECT_LIST[0]} />
+                    {/* 簡短介紹卡片 */}
+                    <IntroCard className="mx-auto max-w-3xl" list={HOME_INTRO_LIST} />
+                </section>
+                {/* 最新文章 */}
+                <section className="flex flex-1 flex-col items-center justify-center gap-y-2">
+                    <div>
+                        {latestPosts?.map((post) => (
+                            <BlogPostCard
+                                key={post?.slug}
+                                className={cn(latestPosts?.length > 1 && 'border-b')}
+                                post={post}
+                            />
+                        ))}
                     </div>
-                </div>
-                {/* 簡短介紹卡片 */}
-                <IntroCard className="mx-auto max-w-3xl" list={HOME_INTRO_LIST} />
-            </section>
-            {/* 最新文章 */}
-            <section className="flex flex-1 flex-col items-center justify-center gap-y-2">
-                <SectionTitle>Latest Post</SectionTitle>
-                <div>
-                    {latestPosts?.map((post) => (
-                        <BlogPostCard
-                            key={post?.slug}
-                            className={cn(latestPosts?.length > 1 && 'border-b')}
-                            post={post}
-                        />
-                    ))}
-                </div>
-                <Link href="/blog/all/1">
-                    <Button variant="outline">View All</Button>
-                </Link>
-            </section>
+                    <Link href="/blog/all/1">
+                        <Button variant="outline">View All</Button>
+                    </Link>
+                </section>
+            </div>
         </div>
     );
 };
