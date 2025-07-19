ff186e26958104e431d492b128a63b8227e8da92
Gary <garylin0969@gmail.com>
2025-07-19 22:49:30 +0800

    feat: enhance ProjectCard component to support badge display and improve null value handling for tags

--- src/components/molecules/project-card/project-card.tsx
+++ src/components/molecules/project-card/project-card.tsx
@@ -7,18 +7,25 @@ import { cn } from '@/utils/shadcn';
 
 interface ProjectCardProps {
     className?: string;
+    badge?: string;
     maxVisible?: number;
     project: (typeof PROJECT_LIST)[number];
 }
 
-const ProjectCard = ({ className, maxVisible, project }: ProjectCardProps) => {
+const ProjectCard = ({ className, badge, maxVisible, project }: ProjectCardProps) => {
     // 限制顯示的標籤數量
-    const visibleTags = project.tags.slice(0, maxVisible ?? project.tags.length);
+    const visibleTags = project?.tags?.slice(0, maxVisible ?? project.tags.length);
     // 計算剩餘的標籤數量
-    const remainingCount = project.tags.length - visibleTags.length;
+    const remainingCount = project?.tags?.length - visibleTags?.length;
 
     return (
-        <a href={project.url} className={cn('group', className)} target="_blank" rel="noreferrer noopener">
+        <a
+            href={project.url}
+            className={cn('group', badge && 'relative', className)}
+            target="_blank"
+            rel="noreferrer noopener"
+        >
+            {badge && <Badge className="absolute -top-2 -right-2 z-10 px-2 py-1">{badge}</Badge>}
             <Card className="flex h-full flex-col gap-0 overflow-hidden p-0">
                 <AspectRatio ratio={16 / 9} className="overflow-hidden">
                     <NextImage
@@ -31,7 +38,7 @@ const ProjectCard = ({ className, maxVisible, project }: ProjectCardProps) => {
                 <CardContent className="flex flex-1 flex-col space-y-6 p-6">
                     <CardTitle className="group-hover:text-primary">{project.name}</CardTitle>
                     <div className="flex flex-wrap items-center gap-2">
-                        {visibleTags.map((tag) => (
+                        {visibleTags?.map((tag) => (
                             <Badge key={tag} className="px-2 py-1" variant="secondary">
                                 {tag}
                             </Badge>
