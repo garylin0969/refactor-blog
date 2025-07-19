efa50d24cdb30d561a03d988ec8d45c4e6ae00d4
Gary <garylin0969@gmail.com>
2025-07-19 23:12:24 +0800

    feat: update HomePage to display latest posts and improve layout with responsive design

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,28 +1,45 @@
+import SectionTitle from '@/components/atoms/section-title';
 import AuthorCard from '@/components/molecules/author-card';
+import BlogPostCard from '@/components/molecules/blog-post-card';
 import IntroCard from '@/components/molecules/intro-card';
 import ProjectCard from '@/components/molecules/project-card';
 import { HOME_INTRO_LIST } from '@/constants/intro';
 import { PROJECT_LIST } from '@/constants/project';
+import { getLatestPosts } from '@/utils/post';
+import { cn } from '@/utils/shadcn';
+
+const latestPosts = getLatestPosts(1);
 
 const HomePage = () => {
     return (
-        <>
-            <div className="mx-auto max-w-6xl space-y-4 md:space-y-8">
-                <section className="space-y-4 md:space-y-8">
-                    <div className="flex items-center justify-center gap-4">
-                        <div className="flex items-center justify-center">
-                            {/* 作者卡片 */}
-                            <AuthorCard />
-                        </div>
-                        <div className="flex items-center justify-center">
-                            <ProjectCard className="w-74" badge="New" maxVisible={4} project={PROJECT_LIST[0]} />
-                        </div>
+        <div className="mx-auto max-w-6xl space-y-4 md:space-y-8">
+            <section className="space-y-4 md:space-y-8">
+                <div className="flex flex-wrap justify-center gap-4">
+                    <div className="flex justify-center">
+                        {/* 作者卡片 */}
+                        <AuthorCard />
                     </div>
-                    {/* 簡短介紹卡片 */}
-                    <IntroCard className="mx-auto max-w-3xl" list={HOME_INTRO_LIST} />
-                </section>
-            </div>
-        </>
+                    <div className="hidden md:flex md:justify-center">
+                        <ProjectCard className="w-74" badge="New Project" maxVisible={4} project={PROJECT_LIST[0]} />
+                    </div>
+                </div>
+                {/* 簡短介紹卡片 */}
+                <IntroCard className="mx-auto max-w-3xl" list={HOME_INTRO_LIST} />
+            </section>
+            {/* 最新文章 */}
+            <section className="flex flex-1 flex-col items-center justify-center">
+                <SectionTitle>Latest Post</SectionTitle>
+                <div>
+                    {latestPosts?.map((post) => (
+                        <BlogPostCard
+                            key={post?.slug}
+                            className={cn(latestPosts?.length > 1 && 'border-b')}
+                            post={post}
+                        />
+                    ))}
+                </div>
+            </section>
+        </div>
     );
 };
 
